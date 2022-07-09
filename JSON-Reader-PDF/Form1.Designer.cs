b1f3d4b178a8a5666f99631cfa46e6b6125e27ea
Sier Santos <louisasantosl12@gmail.com>
2022-07-09 11:55:57 +0900

    Assignment 6

---
+++ JSON-Reader-PDF/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace JSON_Reader_PDF
+{
+    partial class JSONResume
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JSONResume));
+            this.btngenerateJSON = new System.Windows.Forms.Button();
+            this.btnopenFile = new System.Windows.Forms.Button();
+            this.RTBoxResult = new System.Windows.Forms.RichTextBox();
+            this.btnsavePDF = new System.Windows.Forms.Button();
+            this.pcbox1 = new System.Windows.Forms.PictureBox();
+            this.lblConverter = new System.Windows.Forms.Label();
+            this.lblResume = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btngenerateJSON
+            // 
+            this.btngenerateJSON.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btngenerateJSON.BackColor = System.Drawing.Color.LightSalmon;
+            this.btngenerateJSON.Location = new System.Drawing.Point(296, 53);
+            this.btngenerateJSON.Name = "btngenerateJSON";
+            this.btngenerateJSON.Size = new System.Drawing.Size(91, 29);
+            this.btngenerateJSON.TabIndex = 0;
+            this.btngenerateJSON.Text = "Generate JSON";
+            this.btngenerateJSON.UseVisualStyleBackColor = false;
+            this.btngenerateJSON.Click += new System.EventHandler(this.btnreadJSON_Click);
+            // 
+            // btnopenFile
+            // 
+            this.btnopenFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnopenFile.BackColor = System.Drawing.Color.LightSalmon;
+            this.btnopenFile.Location = new System.Drawing.Point(296, 88);
+            this.btnopenFile.Name = "btnopenFile";
+            this.btnopenFile.Size = new System.Drawing.Size(91, 29);
+            this.btnopenFile.TabIndex = 1;
+            this.btnopenFile.Text = "Open and Read";
+            this.btnopenFile.UseVisualStyleBackColor = false;
+            this.btnopenFile.Click += new System.EventHandler(this.btnsavePDF_Click);
+            // 
+            // RTBoxResult
+            // 
+            this.RTBoxResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RTBoxResult.Location = new System.Drawing.Point(12, 53);
+            this.RTBoxResult.Name = "RTBoxResult";
+            this.RTBoxResult.Size = new System.Drawing.Size(278, 256);
+            this.RTBoxResult.TabIndex = 2;
+            this.RTBoxResult.Text = "";
+            // 
+            // btnsavePDF
+            // 
+            this.btnsavePDF.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnsavePDF.BackColor = System.Drawing.Color.LightSalmon;
+            this.btnsavePDF.Location = new System.Drawing.Point(296, 123);
+            this.btnsavePDF.Name = "btnsavePDF";
+            this.btnsavePDF.Size = new System.Drawing.Size(91, 29);
+            this.btnsavePDF.TabIndex = 3;
+            this.btnsavePDF.Text = "Save PDF";
+            this.btnsavePDF.UseVisualStyleBackColor = false;
+            this.btnsavePDF.Click += new System.EventHandler(this.btnsavePDF_Click_1);
+            // 
+            // pcbox1
+            // 
+            this.pcbox1.BackColor = System.Drawing.Color.Transparent;
+            this.pcbox1.Image = ((System.Drawing.Image)(resources.GetObject("pcbox1.Image")));
+            this.pcbox1.Location = new System.Drawing.Point(147, 12);
+            this.pcbox1.Name = "pcbox1";
+            this.pcbox1.Size = new System.Drawing.Size(46, 34);
+            this.pcbox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbox1.TabIndex = 69;
+            this.pcbox1.TabStop = false;
+            // 
+            // lblConverter
+            // 
+            this.lblConverter.AutoSize = true;
+            this.lblConverter.BackColor = System.Drawing.Color.Transparent;
+            this.lblConverter.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConverter.ForeColor = System.Drawing.Color.Maroon;
+            this.lblConverter.Location = new System.Drawing.Point(187, 8);
+            this.lblConverter.Name = "lblConverter";
+            this.lblConverter.Size = new System.Drawing.Size(216, 39);
+            this.lblConverter.TabIndex = 68;
+            this.lblConverter.Text = "CONVERTER";
+            // 
+            // lblResume
+            // 
+            this.lblResume.AutoSize = true;
+            this.lblResume.BackColor = System.Drawing.Color.Transparent;
+            this.lblResume.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResume.ForeColor = System.Drawing.Color.Tomato;
+            this.lblResume.Location = new System.Drawing.Point(5, 8);
+            this.lblResume.Name = "lblResume";
+            this.lblResume.Size = new System.Drawing.Size(153, 39);
+            this.lblResume.TabIndex = 67;
+            this.lblResume.Text = "RESUME";
+            // 
+            // JSONResume
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(399, 321);
+            this.Controls.Add(this.pcbox1);
+            this.Controls.Add(this.lblConverter);
+            this.Controls.Add(this.lblResume);
+            this.Controls.Add(this.btnsavePDF);
+            this.Controls.Add(this.RTBoxResult);
+            this.Controls.Add(this.btnopenFile);
+            this.Controls.Add(this.btngenerateJSON);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "JSONResume";
+            this.Text = "JSON to PDF";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btngenerateJSON;
+        private System.Windows.Forms.Button btnopenFile;
+        private System.Windows.Forms.RichTextBox RTBoxResult;
+        private System.Windows.Forms.Button btnsavePDF;
+        private System.Windows.Forms.PictureBox pcbox1;
+        private System.Windows.Forms.Label lblConverter;
+        private System.Windows.Forms.Label lblResume;
+    }
+}
+
---
+++ JSON-Reader-PDF/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace JSON_Reader_PDF
+{
+    public partial class JSONResume : Form
+    {
+        public JSONResume()
+        {
+            InitializeComponent();
+        }
+        class Resume
+        {
+            public string FirstName { get; set; }
+            public string MiddleInitital { get; set; }
+            public string Surname { get; set; }
+            public string Address { get; set; }
+            public string ZIP { get; set; }
+            public string Email { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Objectives { get; set; }
+            public string College { get; set; }
+            public string SeniorHS { get; set; }
+            public string JuniorHS { get; set; }
+            public string Company1 { get; set; }
+            public string CompanyDes1 { get; set; }
+            public string Company2 { get; set; }
+            public string CompanyDes2 { get; set; }
+            public string Company3 { get; set; }
+            public string CompanyDes3 { get; set; }
+            public List<string> Skills { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("\nName: {0} {1} {2} \nAddress: {3} {4} \nEmail: {5} \nPhone Number: {6} \n\nObjectives: {7} \n\nEducational Attainment \nCollege: {8} \nSenior High School: {9} \nJunior High School: {10} \n\nExperiences \nCompany: {11} \n- {12} \nCompany: {13} \n- {14} \nCompany: {15} \n- {16} \n\nSkills: {17}",
+                        FirstName, MiddleInitital, Surname, Address, ZIP, Email, PhoneNumber, Objectives, College,
+                        SeniorHS, JuniorHS, Company1, CompanyDes1, Company2, CompanyDes2, Company3, CompanyDes3, string.Join(" ", Skills.ToArray()));
+            }
+        }
+
+        private void btnreadJSON_Click(object sender, EventArgs e)
+        {
+            Resume resume = new Resume()
+            {
+                FirstName = "LOUISA REIGHN",
+                MiddleInitital = "A.",
+                Surname = "SANTOS",
+                Address = "Poblacion, San Ildefonso, Bulacan",
+                ZIP = "3010",
+                Email = "louisasantosxx@gmail.com",
+                PhoneNumber = "09XXXXXXXXX",
+                Objectives = " An excellent academic record, ability to understand and test software, working knowledge of Azure, and a strong understanding of core internet technologies. I seek to workas a System Engineer to further my knowledge in the IT domain and utilize my skills",
+                College = "Polytechnic University of the Philippines",
+                SeniorHS = "San Miguel National High School",
+                JuniorHS = "Saint John School",
+                Company1 = "SM Entertainment",
+                CompanyDes1 = "SM Entertainment Co., Ltd. is a South Korean multinational entertainment company. It is on of South Korea's largest entertainment companies where it was established in 1995 by record executive and record producer Lee Soo-man",
+                Company2 = "JYP Entertainment",
+                CompanyDes2 = "JYP Entertainment Corporation is a South Korean multinational entertainment and record label conglomerate founded in 1997 by J. Y. Park.",
+                Company3 = "YG Entertainment",
+                CompanyDes3 = "YG Entertainment Inc. is a South Korean multinational entertainment agency established in 1996 by Yang Hyun-suk. The company operates as a record label, talent agency, music production company, event management and concert production company, and music publishing house.",
+                Skills = new List<string>
+                {
+                    "\n\t" + "*" + "Active listening skills",
+                    "\n\t" + "*" + "Communication skills",
+                    "\n\t" + "*" + "Computer skills",
+                    "\n\t" + "*" + "Management skills",
+                    "\n\t" + "*" + "Leadership skill"
+                }
+            };
+            string file_name = resume.Surname + "_" + resume.FirstName;
+            string saveJSON = JsonConvert.SerializeObject(resume, Formatting.None);
+            File.WriteAllText(@"C:\Users\Santos\Documents\json\" + file_name + ".json", saveJSON);
+            MessageBox.Show("JSON file created." + "\n" + "Please save it as PDF.");
+        }
+
+        private void btnsavePDF_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog() { Filter = "json file|*.json", };
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                string file = openFile.FileName;
+                string json = File.ReadAllText(file);
+                Resume resultResume = JsonConvert.DeserializeObject<Resume>(json);
+                RTBoxResult.Text = resultResume.ToString();
+            }
+            else
+            {
+                MessageBox.Show("No file to open");
+            }
+        }
+
+        private void btnsavePDF_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                var file_image = @"C:\Users\Santos\Pictures\ID\received_196134434820796.jpeg";
+                string rtbresult = RTBoxResult.Text;
+                string[] cutter = new string[] { " " };
+                string[] name_cut = rtbresult.Split(cutter, StringSplitOptions.None);
+                string file_name = name_cut[4] + "_" + name_cut[1] + " " + name_cut[2];
+                using (SaveFileDialog save = new SaveFileDialog() { Filter = "PDF file|*.pdf", ValidateNames = true, FileName = file_name })
+                {
+                    if (RTBoxResult.Text == "")
+                    {
+                        MessageBox.Show("No file to save.");
+                    }
+                    else if (save.ShowDialog() == DialogResult.OK)
+                    {
+                        Document doc = new Document(PageSize.LETTER, 10f, 10f, 10f, 0f);
+                        try
+                        {
+                            PdfWriter.GetInstance(doc, new FileStream(save.FileName, FileMode.Create));
+                            doc.Open();
+                            System.Drawing.Image pImage = System.Drawing.Image.FromFile(file_image);
+
+                            iTextSharp.text.Image ItextImage = iTextSharp.text.Image.GetInstance(pImage, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            ItextImage.ScalePercent(15f);
+                            ItextImage.SetAbsolutePosition(doc.PageSize.Width - 50f - 80f, doc.PageSize.Height - 30f - 100f);
+
+                            doc.Add(ItextImage);
+
+                            iTextSharp.text.Font pfont1 = FontFactory.GetFont(iTextSharp.text.Font.FontFamily.TIMES_ROMAN.ToString(), 20,
+                                iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.BLACK);
+
+                            Paragraph pgraph1 = new Paragraph(name_cut[4] + ", " + name_cut[2] + " " + name_cut[1] + " " + name_cut[3], pfont1);
+                            pgraph1.Alignment = Element.ALIGN_LEFT;
+                            doc.Add(pgraph1);
+
+                            pfont1 = FontFactory.GetFont(iTextSharp.text.Font.FontFamily.TIMES_ROMAN.ToString(), 14,
+                                iTextSharp.text.Font.NORMAL, iTextSharp.text.BaseColor.BLACK);
+                            Paragraph pgraph2 = new iTextSharp.text.Paragraph(RTBoxResult.Text, pfont1);
+                            pgraph2.Alignment = Element.ALIGN_JUSTIFIED;
+                            doc.Add(pgraph2);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        finally
+                        {
+                            doc.Close();
+                            MessageBox.Show("File Saved");
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("No file to save.");
+            }
+        }
+    }
+}
